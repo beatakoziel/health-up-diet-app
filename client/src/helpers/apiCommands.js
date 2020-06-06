0f363efba7a8ce4810eca0b0323ba2af2da3814d
Bartłomiej <46246339+BartekCK@users.noreply.github.com>
2020-06-06 12:28:22 +0200

    Revert "Client"

--- client/src/helpers/apiCommands.js
+++ client/src/helpers/apiCommands.js
@@ -1,11 +1,8 @@
-import {getSafe, post, postSafe} from './api';
-import {getGoalsApiUrl, loginApiUrl, loginApiUrlByFb, postGoalsApiUrl, registerApiUrl, userRoleApiUrl} from './routes';
+import { getSafe, post } from './api';
+import { loginApiUrl, loginApiUrlByFb, registerApiUrl, userRoleApiUrl } from './routes';
 
 export const postCreateUser = body => post(registerApiUrl(), body);
 export const postLoginUser = body => post(loginApiUrl(), body);
 export const postLoginUserByFb = body => post(loginApiUrlByFb(), body);
 
 export const getAndCheckUserRole = () => getSafe(userRoleApiUrl());
-
-export const getGoals = () => getSafe(getGoalsApiUrl());
-export const postGoals = body => postSafe(postGoalsApiUrl(), body);
