902fd78c44a5bc8d17bf6975e771f20a4c4fb262
Beata <beatainez.k@gmail.com>
2020-06-08 22:16:57 +0200

    added more products to database

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -16,3 +16,27 @@ replace into nutrients_per_portion(id, calories, carbohydrates, fat, portion_siz
 values (4, 97, 2, 5, 100, 11, 'GRAMS');
 replace into product(id, category, name, standard_portion_nutrients_id)
 values (4, 'DAIRY', 'Serek wiejski Piątnica', 4);
+replace into nutrients_per_portion(id, calories, carbohydrates, fat, portion_size, proteins, unit)
+values (5, 390, 81, 3.2, 100, 8, 'GRAMS');
+replace into product(id, category, name, standard_portion_nutrients_id)
+values (5, 'GRAIN_PRODUCTS', 'Wafel ryżowy', 5);
+replace into nutrients_per_portion(id, calories, carbohydrates, fat, portion_size, proteins, unit)
+values (6, 882, 0.2, 99.6, 100, 0, 'MILLILITERS');
+replace into product(id, category, name, standard_portion_nutrients_id)
+values (6, 'OILS_AND_FATS', 'Oliwa z oliwek', 6);
+replace into nutrients_per_portion(id, calories, carbohydrates, fat, portion_size, proteins, unit)
+values (7, 258, 51.6, 1.4, 100, 9.3, 'GRAMS');
+replace into product(id, category, name, standard_portion_nutrients_id)
+values (7, 'BREAD', 'Chleb pszenny', 7);
+replace into nutrients_per_portion(id, calories, carbohydrates, fat, portion_size, proteins, unit)
+values (8, 253, 0.9, 20.3, 100, 17.1, 'GRAMS');
+replace into product(id, category, name, standard_portion_nutrients_id)
+values (8, 'MEAT', 'Szynka wieprzowa', 8);
+replace into nutrients_per_portion(id, calories, carbohydrates, fat, portion_size, proteins, unit)
+values (9, 366, 69.3, 7.2, 100, 11.19, 'GRAMS');
+replace into product(id, category, name, standard_portion_nutrients_id)
+values (9, 'GRAIN_PRODUCTS', 'Płatki owsiane górskie', 9);
+replace into nutrients_per_portion(id, calories, carbohydrates, fat, portion_size, proteins, unit)
+values (10, 60, 6.2, 2, 100, 4.3, 'GRAMS');
+replace into product(id, category, name, standard_portion_nutrients_id)
+values (10, 'GRAIN_PRODUCTS', 'Jogurt naturalny', 10);
