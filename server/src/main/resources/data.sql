6869559cbaa12b9e6602db1d3e363421650429eb
Beata <beatainez.k@gmail.com>
2020-06-08 21:55:23 +0200

    bug fix

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -7,8 +7,12 @@ values (1, 'MEAT', 'Pierś z kurczaka', 1);
 replace into nutrients_per_portion(id, calories, carbohydrates, fat, portion_size, proteins, unit)
 values (2, 357, 79, 0.9, 100, 7.9, 'GRAMS');
 replace into product(id, category, name, standard_portion_nutrients_id)
-values (2, 'GRAIN_PRODUCTS', 'Ryż biały Sonko', 1);
+values (2, 'GRAIN_PRODUCTS', 'Ryż biały Sonko', 2);
 replace into nutrients_per_portion(id, calories, carbohydrates, fat, portion_size, proteins, unit)
 values (3, 51, 4.9, 2, 100, 3.3, 'MILLILITERS');
 replace into product(id, category, name, standard_portion_nutrients_id)
-values (3, 'DAIRY', 'Mleko 2%', 1);
+values (3, 'DAIRY', 'Mleko 2%', 3);
+replace into nutrients_per_portion(id, calories, carbohydrates, fat, portion_size, proteins, unit)
+values (4, 97, 2, 5, 100, 11, 'GRAMS');
+replace into product(id, category, name, standard_portion_nutrients_id)
+values (4, 'DAIRY', 'Serek wiejski Piątnica', 4);
