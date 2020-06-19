fef8c48b8186b563ae397c18b4a7e27f6c98a426
Bartek Kotarski <bkotarski1@gmail.com>
2020-06-19 21:36:38 +0200

    Add meals front without API

---
+++ client/src/components/meal-item/MealItem.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Alert, Button, Card, List } from 'antd';
+import { useOpenModal } from '../../hooks/useOpenModal';
+import { GenericModal } from '../Modal';
+
+export const MealItem = ({ idMeal: mealId, name, products, deleteMeal }) => {
+  const [isModalOpen, openModal, closeModal] = useOpenModal();
+
+  const handleDeleteClick = () => {
+    // delete ITEM by ID
+    deleteMeal(mealId);
+  };
+
+  const handleAddClick = () => {
+    // delete ITEM by ID
+    console.log(mealId);
+    openModal();
+  };
+
+  return (
+    <React.Fragment>
+      <Card
+        title={name}
+        extra={
+          <div>
+            <Button type='dashed' danger onClick={handleDeleteClick}>
+              Usuń
+            </Button>
+            <Button type='dashed' onClick={handleAddClick}>
+              Dodaj
+            </Button>
+          </div>
+        }
+        style={{ width: 300 }}
+      >
+        <List
+          size='small'
+          bordered
+          dataSource={products}
+          renderItem={product => (
+            <List.Item>
+              {product.name} {product.quantity} [{product.unit}]
+            </List.Item>
+          )}
+        />
+      </Card>
+      <GenericModal isShow={isModalOpen} handleClose={closeModal}>
+        <Alert
+          message='Oznaczono'
+          description='Danie zostalo dodane do spożytych produktów'
+          type='success'
+          showIcon
+        />
+      </GenericModal>
+    </React.Fragment>
+  );
+};
